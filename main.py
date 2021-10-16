1277f0e6b6efe8b37eb8d21490a1941627e72a71
ibrahim <oladimejiibrahim61@gmail.com>
2021-10-16 13:56:47 +0100

    Add Procfile amended1

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ from functools import wraps
 import os
 
 
-port = int(os.environ.get('PORT', 5000))
-
 
 from flask import abort
 
@@ -221,4 +219,5 @@ def delete_post(post_id):
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
