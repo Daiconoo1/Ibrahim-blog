24f0f0530b7bb329c9f7b02672513c1864dd5bca
ibrahim <oladimejiibrahim61@gmail.com>
2021-10-17 14:06:50 +0100

    Procfile test9

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
-
+# 'sqlite:///blog.db'
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
